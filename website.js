20bf2e918d9d8909bab0d45389fc5e1acb3e7f48
Dluffy17 <123863790+Dluffy17@users.noreply.github.com>
2023-01-29 22:43:07 -0600

    Update website.js

--- website.js
+++ website.js
@@ -1,4 +1,4 @@
-<script>
+
       const title = document.getElementById("title");
       const text = document.getElementById("text");
 
@@ -8,5 +8,5 @@
         title.style.color = count % 2 === 0 ? "olive" : "red";
         text.style.color = count % 2 === 0 ? "olive" : "red";
       }, 1000);
-    </script>
+
 
