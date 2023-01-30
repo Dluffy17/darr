0f7bf315f3b6b816e7094d0c0017e233050c688c
Dluffy17 <123863790+Dluffy17@users.noreply.github.com>
2023-01-29 22:26:41 -0600

    Update website.js

--- website.js
+++ website.js
@@ -1,32 +1,13 @@
-// Add responsive behavior for navigation menu
-const nav = document.querySelector("nav");
-const menuBtn = document.querySelector(".menu-btn");
-let menuOpen = false;
-menuBtn.addEventListener("click", function() {
-  if (!menuOpen) {
-    nav.style.display = "block";
-    menuBtn.innerHTML = "Close";
-    menuOpen = true;
-  } else {
-    nav.style.display = "none";
-    menuBtn.innerHTML = "Menu";
-    menuOpen = false;
-  }
-});
+<script>
+      const title = document.getElementById("title");
+      const text = document.getElementById("text");
 
-// Add animation to header text
-const header = document.querySelector("header");
-const main = document.querySelector("main");
-const footer = document.querySelector("footer");
-header.style.opacity = "0";
-main.style.opacity = "0";
-footer.style.opacity = "0";
-
-setTimeout(function() {
-  header.style.opacity = "1";
-  header.style.transform = `translateY(0)`;
-  main.style.opacity = "1";
-  main.style.transform = `translateY(0)`;
-  footer.style.opacity = "1";
-  footer.style.transform = `translateY(0)`;
-}, 500);
+      let count = 0;
+      setInterval(() => {
+        count++;
+        title.style.color = count % 2 === 0 ? "olive" : "red";
+        text.style.color = count % 2 === 0 ? "olive" : "red";
+      }, 1000);
+    </script>
+  </body>
+</html>
